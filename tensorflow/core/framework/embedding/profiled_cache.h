af1dba9105832063f0eb5253c0570f486748cd83
RTXUX <wyf@rtxux.xyz>
2023-08-11 15:48:56 +0800

    [Embedding] add profiler profiling code

--- tensorflow/core/framework/embedding/profiled_cache.h
+++ tensorflow/core/framework/embedding/profiled_cache.h
@@ -1,6 +1,8 @@
 #ifndef DEEPREC_PROFILED_CACHE_H
 #define DEEPREC_PROFILED_CACHE_H
 
+#include <chrono>
+
 #include "tensorflow/core/framework/embedding/cache_profiler.h"
 #include "tensorflow/core/framework/embedding/cache_manager.h"
 
@@ -18,20 +20,27 @@ public:
 
     }
 
-    void add_to_cache(const K *batch_ids, const size_t batch_size) override {
-      LRUCache<K>::add_to_cache(batch_ids, batch_size);
-      profiler_.ReferenceKeyBatch(batch_ids, batch_size);
-      CacheManager<K>::GetInstance().Access();
-    }
+//    void add_to_cache(const K *batch_ids, const size_t batch_size) override {
+//      LRUCache<K>::add_to_cache(batch_ids, batch_size);
+//      profiler_.ReferenceKeyBatch(batch_ids, batch_size);
+//      CacheManager<K>::GetInstance().Access();
+//    }
 
     SamplingLRUAETProfiler<K> *GetProfiler() {
       return &profiler_;
     }
 
     void update(const K *batch_ids, size_t batch_size, bool use_locking) override {
+      using Clock = std::chrono::high_resolution_clock;
+      auto start = Clock::now();
       LRUCache<K>::update(batch_ids, batch_size, use_locking);
+      auto end_base = Clock::now();
       profiler_.ReferenceKeyBatch(batch_ids, batch_size);
+      auto end_profiler = Clock::now();
       CacheManager<K>::GetInstance().Access();
+      auto lru_time = std::chrono::duration_cast<std::chrono::nanoseconds>(end_base - start).count();
+      auto profiler_time = std::chrono::duration_cast<std::chrono::nanoseconds>(end_profiler - end_base).count();
+      CacheManager<K>::GetInstance().IncreaseNanos(lru_time, profiler_time);
     }
 
     ~ProfiledLRUCache() override {
