277e3d0e3853647bf71294a1ebb7f8fb59b3a060
RTXUX <wyf@rtxux.xyz>
2023-11-06 15:14:36 +0800

    [Embedding] Add profiled sharded cache

--- tensorflow/core/framework/embedding/cache_factory.h
+++ tensorflow/core/framework/embedding/cache_factory.h
@@ -27,6 +27,7 @@ class CacheFactory {
     template<typename K>
     static BatchCache<K> *
     Create(CacheStrategy cache_strategy, std::string name, TunableCache *tunable_cache = nullptr) {
+      int64 shard_shift;
       switch (cache_strategy) {
         case CacheStrategy::LRU:
           LOG(INFO) << " Use Storage::LRU in multi-tier EmbeddingVariable "
@@ -52,10 +53,21 @@ class CacheFactory {
           }
           return cache;
         case CacheStrategy::ShardedLRU:
-          LOG(INFO) << " Use Storage::LFU in multi-tier EmbeddingVariable " << name;
-          int64 shard_shift;
+          LOG(INFO) << " Use Storage::ShardedLRU in multi-tier EmbeddingVariable " << name;
           ReadInt64FromEnvVar("CACHE_SHARD_SHIFT", 0, &shard_shift);
           return new ShardedLRUCache<K>(name, shard_shift);
+        case CacheStrategy::ProfiledShardedLRU:
+          LOG(INFO) << " Use Storage::ProfiledShardedLRU in multi-tier EmbeddingVariable " << name;
+          ReadInt64FromEnvVar("CACHE_SHARD_SHIFT", 0, &shard_shift);
+          ReadInt64FromEnvVar("CACHE_PROFILER_BUCKET_SIZE", 10, reinterpret_cast<int64 *>(&bucket_size));
+          ReadInt64FromEnvVar("CACHE_PROFILER_MAX_REUSE_DIST", 100000, reinterpret_cast<int64 *>(&max_reuse_dist));
+          ReadInt64FromEnvVar("CACHE_PROFILER_SAMPLING_INTERVAL", 1, reinterpret_cast<int64 *>(&sampling_interval));
+          ProfiledShardedLRUCache<K> *pscache;
+          pscache = new ProfiledShardedLRUCache<K>(name, bucket_size, max_reuse_dist, sampling_interval, shard_shift, tunable_cache);
+          if (tunable_cache != nullptr) {
+            CacheManager::GetInstance().RegisterCache(*pscache->GetProfiler());
+          }
+          return pscache;
         default:
           LOG(INFO) << " Invalid Cache strategy, \
                        use LFU in multi-tier EmbeddingVariable "
--- tensorflow/core/framework/embedding/profiled_cache.h
+++ tensorflow/core/framework/embedding/profiled_cache.h
@@ -3,6 +3,7 @@
 
 #include <chrono>
 
+#include "tensorflow/core/framework/embedding/cache.h"
 #include "tensorflow/core/framework/embedding/cache_manager.h"
 #include "tensorflow/core/framework/embedding/cache_profiler.h"
 
@@ -17,7 +18,8 @@ class ProfiledLRUCache : public LRUCache<K> {
                             TunableCache* tunable_cache = nullptr)
       : LRUCache<K>(name),
         profiler_(name, bucket_size, max_reuse_time, sampling_interval,
-                  tunable_cache), entry_size(tunable_cache->GetCacheEntrySize()) {}
+                  tunable_cache),
+        entry_size(tunable_cache->GetCacheEntrySize()) {}
 
   //    void add_to_cache(const K *batch_ids, const size_t batch_size) override
   //    {
@@ -50,6 +52,51 @@ class ProfiledLRUCache : public LRUCache<K> {
   }
 
   ~ProfiledLRUCache() override {
+    CacheManager::GetInstance().UnregisterCache(profiler_.GetName());
+  }
+
+ private:
+  SamplingLRUAETProfiler<K> profiler_;
+  size_t entry_size;
+};
+
+template <typename K>
+class ProfiledShardedLRUCache : public ShardedLRUCache<K> {
+ public:
+  explicit ProfiledShardedLRUCache(const std::string& name, const size_t bucket_size,
+                         const size_t max_reuse_time,
+                         const uint64_t sampling_interval,
+                         const int shard_shift,
+                         TunableCache* tunable_cache = nullptr)
+    : ShardedLRUCache<K>(name, shard_shift),
+    profiler_(name, bucket_size, max_reuse_time, sampling_interval,
+              tunable_cache),
+    entry_size(tunable_cache->GetCacheEntrySize()) {}
+
+  SamplingLRUAETProfiler<K>* GetProfiler() { return &profiler_; }
+
+  void update(const K* batch_ids, size_t batch_size,
+              bool use_locking) override {
+    using Clock = std::chrono::high_resolution_clock;
+    auto start = Clock::now();
+    ShardedLRUCache<K>::update(batch_ids, batch_size, use_locking);
+    auto end_base = Clock::now();
+    if (CacheManager::GetInstance().SamplingActive()) {
+      profiler_.ReferenceKeyBatch(batch_ids, batch_size);
+    }
+    auto end_profiler = Clock::now();
+    const size_t access_size = batch_size * entry_size;
+    CacheManager::GetInstance().Access(access_size);
+    auto lru_time =
+        std::chrono::duration_cast<std::chrono::nanoseconds>(end_base - start)
+            .count();
+    auto profiler_time = std::chrono::duration_cast<std::chrono::nanoseconds>(
+                             end_profiler - end_base)
+                             .count();
+    CacheManager::GetInstance().IncreaseNanos(lru_time, profiler_time);
+  }
+
+  ~ProfiledShardedLRUCache() {
     CacheManager::GetInstance().UnregisterCache(profiler_.GetName());
   }
 
